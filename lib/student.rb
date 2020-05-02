7902208252168d2fe46c614cc8424ca2893f3212
github username <github email address>
2020-05-01 21:19:20 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User #class student is inheriting from user class = all methods coded in user
+   #im the student and user is the parent class who's funneling to student
+    attr_accessor :knowledge #user gives functions to the student 
+    #student inherits from user 
+    #left side inherits from right side
 
+    def initialize 
+        @knowledge = [] 
+    end
+def learn(string)
+    knowledge << string
+end 
 end
